f16863dce111131566fabf90d7611f779db65266
indu kushwaha <goforindu@gmail.com>
2022-09-13 17:27:52 +0530

    props used

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import './App.css';
 import User from './components/User';
 
 function App() {
+  const user=[{
+    name:"indu kushwaha",
+    email:"goforindu@gmail.com",
+    profile_pic:"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/61f1b112-2cb0-40b0-a6b9-9ab92066e5c7/d33mwsf-0dd81126-6d91-4b0d-905c-886a1a41566c.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwic3ViIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsImF1ZCI6WyJ1cm46c2VydmljZTpmaWxlLmRvd25sb2FkIl0sIm9iaiI6W1t7InBhdGgiOiIvZi82MWYxYjExMi0yY2IwLTQwYjAtYTZiOS05YWI5MjA2NmU1YzcvZDMzbXdzZi0wZGQ4MTEyNi02ZDkxLTRiMGQtOTA1Yy04ODZhMWE0MTU2NmMucG5nIn1dXX0.1vwL8FOjsywblgj9w4zBe3Cakiyvmmi412HCfZgfM60"
+  },
+{
+  name:"Advik Kushwaha",
+  email:"advik@gmail.com",
+  profile_pic:"https://www.tuktukdesign.com/wp-content/uploads/2020/01/employee-icon-vector.jpg"
+}]
   return (
     <div className="App">
-       <User/>
-       <User/>
+       <User name={user[0].name} email={user[0].email} profile_pic={user[0].profile_pic}/>
+       <User name={user[1].name} email={user[1].email} profile_pic={user[1].profile_pic}/>
     </div>
   );
 }
--- src/components/User.js
+++ src/components/User.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import profile_pic from '../images/profile_pic.png';
 
-function User(){
+const User=props=>{
     return(
         <div className="container mt-3">
-        
-        <div className="card bg-light" style={{width:'250px'}}>
-        <div className="card-body">
-          <img className="card-img-left" src={profile_pic} alt="Card image" style={{width:'50%'}}/>
-          
-            <h4 className="card-title">User Profile</h4>
+          <div className="card bg-light" style={{width:'250px'}}>
+          <div className="card-body">
+          <img className="card-img-left" src={props.profile_pic} alt="Card image" style={{width:'50%'}}/>
+           <h4 className="card-title">User Profile</h4>
             <p className="card-text">
-             <p>Indu Kushwaha</p>
-              <p>goforindu@gmail.com</p>
+             <p>{props.name}</p>
+              <p>{props.email}</p>
             </p>
             <a href="#" class="btn btn-primary">See Details</a>
-           
-          </div>
+           </div>
         </div>
         <br/>
         </div>
